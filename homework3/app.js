3a3581a347c4026a548281df622d0cf299dd8878
Dimitar <dimemilkov1@gmail.com>
2021-09-14 16:51:20 +0200

    homework3

---
+++ homework3/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let countries = [];
+
+app
+  .post("/countries", (req, res) => {
+    countries.push(req.body);
+    res.send(countries);
+  })
+  .delete("/countries/:id", (req, res) => {
+    for (let i = 0; i < countries.length; i++) {
+      if (countries[i].id == req.params.id) {
+        countries.splice(i, 1);
+      }
+    };
+    res.send(countries);
+  })
+  .get("/countries", (req, res) => {
+    res.send(countries);
+  })
+  .patch("/countries/:id", (req, res) => {
+    countries.forEach(country => {
+      if (country.id == req.params.id) {
+        if (req.body.name) {
+          country.name = req.body.name;
+        }
+        if (req.body.lang) {
+          country.lang = req.body.lang;
+        }
+      }
+    });
+    res.send(countries);
+  })
+  .put("/countries/:id", (req, res) => {
+    for (let i = 0; i < countries.length; i++) {
+      if (countries[i].id == req.params.id) {
+        countries[i] = req.body;
+      }
+    };
+    res.send(countries);
+  });
+app.listen("3003");
