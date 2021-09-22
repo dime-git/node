7837ea474a4667e542b0ec501ae8f4049c9c7fdc
Dimitar <dimemilkov1@gmail.com>
2021-09-22 19:09:51 +0200

    homework5

--- homework5/routes/index.js
+++ homework5/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
-let names = ['California', "Florida", "United Kingdom"];
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express', people: names });
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/countries', (req, res) => {
+  res.render('countries', {
+    countries: [
+      {
+        name: 'California',
+        city: 'Los Angeles',
+        population: '39 510 000'
+      },
+      {
+        name: 'Florida',
+        city: 'Miami',
+        population: '21 480 000'
+      },
+      {
+        name: 'United Kingdom',
+        city: 'London',
+        population: '66 650 000'
+      }
+    ]
+  });
 });
 
 module.exports = router;
