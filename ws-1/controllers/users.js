6febf6ec6cfa81d6b9bc9034f29bd0db8f21b6a6
Dimitar <dimemilkov1@gmail.com>
2021-10-21 18:56:13 +0200

    login/register

---
+++ ws-1/controllers/users.js
@@ -0,0 +1,62 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    register: async (req, res) => {
+        try {
+            let user = await User.findOne({ email: req.body.email })
+            if (user) {
+                throw new Error('This email is already taken!');
+            }
+
+            //encrypting password
+            req.body.password = bcrypt.hashSync(req.body.password);
+            user = await User.create(req.body);
+
+            res.send({
+                error: false,
+                message: 'New user record created!',
+                user: user
+            });
+        } catch (error) {
+            res.send({
+                error: true,
+                message: error.message
+            });
+        }
+    },
+    login: async (req, res) => {
+        try {
+            const user = await User.findOne({ email:req.body.email });
+            if(!user) {
+                throw new Error('Invalid credentials!');
+            }
+
+            if(!bcrypt.compareSync(req.body.password, user.password)) {
+                throw new Error('Invalid credentials!!');
+            }
+            
+            const payload = {
+                id: user._id,
+                email: user.email
+            }
+
+            const token = jwt.sign(payload, 'DSAJDA(E)#NRA1231', {
+                expiresIn: '50m'
+            });
+
+            res.send({
+                error: false,
+                message: 'User loggden in',
+                token: token
+            });
+        } catch(error) {
+            res.send({
+                error: true,
+                message: error.message
+            });
+        }
+    }
+
+}
