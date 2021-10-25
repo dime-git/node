86c4ceee680bf2f601c651e5950034dd373452aa
Dimitar <dimemilkov1@gmail.com>
2021-10-25 17:36:25 +0200

    Added /delete route

--- ws-1/app.js
+++ ws-1/app.js
@@ -3,10 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -20,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 module.exports = app;
