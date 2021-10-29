1a7d7d2ca757dec5672115f3a4542fa3130807ff
Dimitar <dimemilkov1@gmail.com>
2021-10-29 17:35:09 +0200

    Protecting routes

--- ws-1/routes/products.js
+++ ws-1/routes/products.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const controller = require('../controllers/products');
-// const jwt = require('express-jwt');
+const jwt = require('express-jwt');
 
-// require('dotenv').config();
+require('dotenv').config();
 
 router
-    .post('/', controller.create)
-    .put('/:id', controller.update)
+    .post('/', jwt({ secret: process.env.AUTH_SECRET, algorithms: ['HS256'] }), controller.create)
+    .put('/:id', jwt({ secret: process.env.AUTH_SECRET, algorithms: ['HS256'] }), controller.update)
     .delete('/:id', controller.delete)
     .get('/', controller.all)
-    .get('/categories/:id', controller.byCategory)
-    .get('/:id', controller.byProduct) 
+    .get('/categories/:id', jwt({ secret: process.env.AUTH_SECRET, algorithms: ['HS256'] }), controller.byCategory)
+    .get('/:id', jwt({ secret: process.env.AUTH_SECRET, algorithms: ['HS256'] }), controller.byProduct)
 
 module.exports = router;
